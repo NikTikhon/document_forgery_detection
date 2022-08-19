976115a103a7fad421d237ed4c211ee9f1626ac3
NikTikhon <89089493+NikTikhon@users.noreply.github.com>
2022-08-19 20:29:05 +0400

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -13,32 +13,32 @@ import argparse
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3, 1, padding=1)
+        self.conv1 = nn.Conv2d(3, 32, 3, 1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
         nn.init.xavier_uniform_(self.conv1.weight)
 
-        self.conv2 = nn.Conv2d(32, 32, 3, 1, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, 1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(32)
         nn.init.xavier_uniform_(self.conv2.weight)
 
-        self.conv3 = nn.Conv2d(32, 64, 3, 1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, 1, padding=1, bias=False)
         self.bn3 = nn.BatchNorm2d(64)
         nn.init.xavier_uniform_(self.conv3.weight)
 
-        self.conv4 = nn.Conv2d(64, 64, 3, 1, padding=1)
+        self.conv4 = nn.Conv2d(64, 64, 3, 1, padding=1, bias=False)
         self.bn4 = nn.BatchNorm2d(64)
         nn.init.xavier_uniform_(self.conv4.weight)
 
-        self.conv5 = nn.Conv2d(64, 128, 3, 1, padding=1)
+        self.conv5 = nn.Conv2d(64, 128, 3, 1, padding=1, bias=False)
         self.bn5 = nn.BatchNorm2d(128)
         nn.init.xavier_uniform_(self.conv5.weight)
 
-        self.conv6 = nn.Conv2d(128, 128, 3, 1, padding=1)
+        self.conv6 = nn.Conv2d(128, 128, 3, 1, padding=1, bias=False)
         self.bn6 = nn.BatchNorm2d(128)
         nn.init.xavier_uniform_(self.conv6.weight)
         self.bn7 = nn.BatchNorm1d(128)
 
-        self.fc1 = nn.Linear(8192, 128)
+        self.fc1 = nn.Linear(8192, 128, bias=False)
         self.fc2 = nn.Linear(128, 2)
         nn.init.xavier_uniform_(self.fc1.weight)
         nn.init.xavier_uniform_(self.fc2.weight)
